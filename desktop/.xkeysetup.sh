b1f8cc35f0475d04a0a9685bbeded4bf33edb2fc
Laurent Bachelier <laurent@bachelier.name>
2010-07-11 15:19:01 +0200

    moved more stuff to the automated X init
    and support for a local xinitrc

--- desktop/.xkeysetup.sh
+++ desktop/.xkeysetup.sh
@@ -16,4 +16,8 @@ xmodmap ~/.Xmodmap
 # reload xbindkeys or start it
 killall -HUP xbindkeys||xbindkeys
 
+# make the center button acts as a third button to avoid using the wheel
 xinput set-button-map "Kingsis Peripherals  Evoluent VerticalMouse 3" 1 2 2 4 5 6 7 3 8
+
+# disable the touchpad on laptops
+xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 8 0
