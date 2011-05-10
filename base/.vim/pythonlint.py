0a3cff3c5e8c60acccba44231b2c0bcb216a68a7
Laurent Bachelier <laurent@bachelier.name>
2011-05-10 20:19:03 +0200

    Add support for the pep8 tool, and ignore its ImportWarnings

--- base/.vim/pythonlint.py
+++ base/.vim/pythonlint.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import warnings
 with warnings.catch_warnings():
-    warnings.filterwarnings("ignore",category=DeprecationWarning)
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
     from pyflakes.scripts.pyflakes import main
-main()
+try:
+    main()
+except SystemExit, e:
+    pass
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=ImportWarning)
+    from pkg_resources import load_entry_point
+load_entry_point('pep8', 'console_scripts', 'pep8')()
+
+raise e
