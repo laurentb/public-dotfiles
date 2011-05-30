66ed896e2e58aab1f2d1d0628719b9703d51f8cb
Laurent Bachelier <laurent@bachelier.name>
2011-05-30 10:02:33 +0200

    No spaces in extension list

--- desktop/.urxvt/xrdb.sh
+++ desktop/.urxvt/xrdb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -xeu
 xrdb -merge <<END
 URxvt*perl-lib: ${HOME}/.urxvt/perl
-URxvt*perl-ext-common: default, $(ls -m ${HOME}/.urxvt/perl)
+URxvt*perl-ext-common: default,$(ls -m ${HOME}/.urxvt/perl|sed 's/ //g')
 END
