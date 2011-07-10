9677e8ac35ad59c805036ca64351f47bea072434
Laurent Bachelier <laurent@bachelier.name>
2011-07-10 14:23:04 +0200

    Various make.conf enhancements

--- gentoo/portage/make_conf/arch.p.py
+++ gentoo/portage/make_conf/arch.p.py
@@ -6,6 +6,10 @@ with open("/proc/cpuinfo", "r") as cpuinfo:
     # HACK: pni means sse3
     flags = [ 'sse3' if flag == 'pni' else flag for flag in flags ]
 
+with open("/proc/cpuinfo", "r") as cpuinfo:
+    procs = re.findall("^processor\s+:\s+(\d+)$", cpuinfo.read(), re.MULTILINE)
+    jobs = len(procs) + 1
+
 available_use_flags = ( "avx", "mmx", "mmxext", "3dnow", "3dnowext", "sse", "sse2", "sse3", "sse4", "ssse3" )
 
 use_flags = [ flag for flag in available_use_flags if flag in flags ]
@@ -17,9 +21,11 @@ cflags = subprocess.Popen(gcc, shell=True, stdout=subprocess.PIPE).communicate()
 if __name__ == "__main__":
     print use_flags
     print cflags
+    print jobs
     exit()
 
 text("""# $warning
 USE_ARCH="$flags"
 ARCH_FLAGS="$cflags"
-""").render(flags=" ".join(use_flags), cflags=cflags)
+ARCH_JOBS="$jobs"
+""").render(flags=" ".join(use_flags), cflags=cflags, jobs=jobs)
