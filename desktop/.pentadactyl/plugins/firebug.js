dfd7c55caf03e3ac156a8a8f2f30d9e7c2024cf9
Laurent Bachelier <laurent@bachelier.name>
2012-01-08 11:22:00 +0100

    Update Firebug plugin

--- desktop/.pentadactyl/plugins/firebug.js
+++ desktop/.pentadactyl/plugins/firebug.js
@@ -1,114 +1,151 @@
-/**
- * VERSION: 0.1
- * AUTHOR: Maksim Ryzhikov
- * Plugin to interact with firebug.
- * Based on plugin firebug for vimperator
- */
-
-var FirebugPentadactyl = function () {
-	var fb = Firebug,
-	fbCommandLine = fb.CommandLine;
-	return {
-		_initialize: function () {
-			var self = this;
-			if (!this._initialized) {
-				document.getElementById('fbCommandEditor').addEventListener('blur', function (event) {
-					self.console_run();
-				},
-				true);
-				this._initialized = true;
-			}
-			return this;
-		},
-		_exec: function (args, query) {
-			var self = this;
-			args.forEach(function (cmd) {
-				self[cmd.replace('-', '_')](query);
-			});
-		},
-		_initialized: false,
-		open: function () {
-			if (!fb.chrome.isOpen()) {
-				fb.toggleBar(true, 'console');
-			}
-			setTimeout(function () {
-				var browser = fb.chrome.getCurrentBrowser();
-				browser.chrome.getSelectedPanel().document.defaultView.focus();
-			},
-			100);
-		},
-		off: function () {
-			fb.closeFirebug(true);
-		},
-		close: function () {
-			if (fb.chrome.isOpen()) {
-				fb.toggleBar();
-			}
-		},
-		toggle: function () {
-			fb.toggleBar();
-		},
-		previous_tab: function () {
-			fb.chrome.gotoPreviousTab();
-		},
-		next: function (query) {
-			var tab = query[0]['-tab'] || query[0]['-T'];
-			return (tab ? fb.chrome.switchToPanel(fb.currentContext, tab) : fb.chrome.gotoSiblingTab(!tab));
-		},
-		console_run: function () {
-			if (fb.chrome.isOpen()) {
-				fbCommandLine.enter(fb.currentContext);
-			}
-		},
-		console_clear: function () {
-			if (fb.chrome.isOpen()) {
-				fb.Console.clear();
-			}
-		},
-		console_focus: function () {
-			if (!fb.chrome.isOpen()) {
-				this.open();
-			}
-			fb.chrome.switchToPanel(fb.currentContext, "console");
-			var cmLine = fbCommandLine.getSingleRowCommandLine(),
-			cmEditor = fbCommandLine.getCommandEditor(),
-			commandLine = fb.commandEditor ? cmEditor: cmLine;
-
-			setTimeout(function () {
-				commandLine.select();
-			},
-			100);
-		}
-	};
-};
-
-var fbp = (new FirebugPentadactyl())._initialize();
-
-group.commands.add(['firebug', 'fbp'], 'Control firebug from within pentadactyl.', function (args) {
-	fbp._exec(args, arguments);
-},
-{
-	count: true,
-	argCount: '*',
-	completer: function (context) {
-		var cmds = [],
-		cmd;
-		for (cmd in fbp) {
-			if (fbp.hasOwnProperty(cmd) && cmd.indexOf('_') !== 0) {
-				cmds.push([cmd.replace('_', '-'), '']);
-			}
-		}
-		context.completions = cmds;
-	},
-	options: [{
-		names: ["-tab", "-T"],
-		description: "Select Tab",
-		type: commands.OPTION_STRING,
-		completer: function (context) {
-			var array = ['console', 'html', 'dom', 'css', 'net', 'script'];
-			context.completions = array.map(function (item) {
-				return [item, ''];
-			});
-		}
-	}]
-});
+(function() {
+  "use strict";
+  var chrome, cmd, fb, firebug;
+  var __slice = Array.prototype.slice;
+  fb = Firebug;
+  chrome = fb.chrome;
+  cmd = fb.CommandLine;
+  firebug = {
+    info: {
+      version: '0.1.2',
+      open: "open firebug window",
+      close: "minimize firebug window",
+      toggle: "toggle firebug window",
+      disable: "exit from firebug",
+      console: "open console and set focus",
+      multiline: "multiline console",
+      "toggle-console": "toggle between one-line and multiline console",
+      clear: "clear console output window",
+      run: "run script that was entered in console editor",
+      'tab': "focuses the specified firebug tab (console, html, stylesheet, script, dom, net, etc)",
+      'tab-side': "focuses the specified firebug side tab (css, computed, layout, dom, domSide, watch)",
+      '>': "focuses the next firebug tab(right)",
+      '<': "focuses the next firebug tab(left)",
+      '#': "focuses the prev firebug tab",
+      '/': "search",
+      inspect: "toggle the firebug element inspector"
+    },
+    open: function() {
+      if (!chrome.isOpen()) {
+        return fb.toggleBar(true, 'console');
+      }
+    },
+    close: function() {
+      if (chrome.isOpen()) {
+        return fb.toggleBar();
+      }
+    },
+    toggle: function() {
+      return fb.toggleBar();
+    },
+    disable: function() {
+      return fb.closeFirebug(true);
+    },
+    inspect: function() {
+      if (!fb.currentContext) {
+        fb.toggleBar(true);
+      }
+      return fb.Inspector.toggleInspecting(fb.currentContext);
+    },
+    console: function() {
+      var cmEditor, cmLine;
+      if (!chrome.isOpen()) {
+        this.open();
+      }
+      chrome.switchToPanel(fb.currentContext, "console");
+      cmLine = cmd.getSingleRowCommandLine();
+      cmEditor = cmd.getCommandEditor();
+      return (fb.commandEditor ? cmEditor : cmLine).select();
+    },
+    multiline: function() {
+      if (chrome.isOpen()) {
+        return cmd.toggleMultiLine(true);
+      }
+    },
+    "toggle-console": function() {
+      if (chrome.isOpen()) {
+        return cmd.toggleMultiLine();
+      }
+    },
+    clear: function() {
+      if (chrome.isOpen()) {
+        return fb.Console.clear();
+      }
+    },
+    run: function() {
+      if (chrome.isOpen()) {
+        return cmd.enter(fb.currentContext);
+      }
+    },
+    'tab': function(panelName) {
+      if (panelName == null) {
+        panelName = "console";
+      }
+      if (chrome.isOpen()) {
+        return chrome.navigate(null, panelName);
+      }
+    },
+    'tab-side': function(panelName) {
+      if (chrome.isOpen()) {
+        return chrome.selectSidePanel(panelName);
+      }
+    },
+    '>': function() {
+      if (chrome.isOpen()) {
+        return chrome.gotoSiblingTab(true);
+      }
+    },
+    '<': function() {
+      if (chrome.isOpen()) {
+        return chrome.gotoSiblingTab();
+      }
+    },
+    '#': function() {
+      if (chrome.isOpen()) {
+        return chrome.gotoPreviousTab();
+      }
+    },
+    '/': function() {
+      var text;
+      text = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      if (chrome.isOpen()) {
+        fb.Search.focus(fb.currentContext);
+        return fb.Search.search(text.join(' '), fb.currentContext);
+      }
+    }
+  };
+  group.commands.add(['firebug', 'fbpc'], "firebug-pentadactyl (version " + firebug.info.version + ")", function(args) {
+    var command, index, _len, _results;
+    _results = [];
+    for (index = 0, _len = args.length; index < _len; index++) {
+      command = args[index];
+      if (firebug[command]) {
+        _results.push(firebug[command].apply(firebug, args.slice(index + 1)));
+      }
+    }
+    return _results;
+  }, {
+    completer: function(context) {
+      var name;
+      return context.completions = (function() {
+        var _results;
+        _results = [];
+        for (name in firebug) {
+          if (name !== 'info' || name.indexOf('_')) {
+            _results.push([name, firebug.info[name]]);
+          }
+        }
+        return _results;
+      })();
+    }
+  });
+  group.options.add(["fbliverun"], "run script on blur firebug console", "boolean", false, {
+    setter: function(value) {
+      if (value) {
+        return group.events.listen(cmd.getCommandEditor(), 'blur', firebug.run, true);
+      } else if (!value) {
+        return group.events.unlisten(cmd.getCommandEditor(), 'blur', firebug.run, true);
+      }
+    }
+  });
+}).call(this);
