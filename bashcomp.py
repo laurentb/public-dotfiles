94d0802e1f4bbd878da425e3beb678e32b5ce767
Laurent Bachelier <laurent@bachelier.name>
2012-01-08 23:08:37 +0100

    Better display

--- bashcomp.py
+++ bashcomp.py
@@ -2,6 +2,11 @@
 from __future__ import with_statement
 from os import path, popen, system
 import re
+import sys
+
+interactive = sys.stdin.isatty()
+BRIGHT = '\x1b[1m' if interactive else ''
+NORMAL = '\x1b[22m' if interactive else ''
 
 config_file = path.join(path.dirname(__file__), "bashcomp.wanted")
 
@@ -27,7 +32,7 @@ with open(config_file, "r") as f:
 completions = set(wanted_completions) - set(active_completions)
 for completion in completions:
     if completion in available_completions:
-        print "+%s" % completion
+        print "%s+%s%s" % (BRIGHT, NORMAL, completion),
         system("eselect bashcomp enable %s" % completion)
     else:
-        print "-%s" % completion
+        print "%s-%s%s" % (BRIGHT, NORMAL, completion),
