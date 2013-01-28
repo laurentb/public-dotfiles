8b138f003969e192dc6d4cfe0f45426ef2d7bca1
Laurent Bachelier <laurent@bachelier.name>
2013-01-28 14:50:13 +0100

    Fix run when not in dir

--- fontconfig.sh
+++ fontconfig.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -xeu
+cd $(dirname $0)
 [[ -f /usr/share/eselect/modules/fontconfig.eselect ]] && ./fontconfig.py
 [[ -f /usr/share/eselect/modules/infinality.eselect ]] && eselect infinality set nyx && eselect lcdfilter set nyx
