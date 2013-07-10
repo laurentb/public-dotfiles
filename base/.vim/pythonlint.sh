01d42f2cb82d92cb72933d53ccf90d808908f0f2
Laurent Bachelier <laurent@bachelier.name>
2013-07-10 15:02:01 +0200

    Fixes for latest flake8

--- base/.vim/pythonlint.sh
+++ base/.vim/pythonlint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ue
 # fail on pyflakes errors
-flake8 --ignore=E,W "$@"
+flake8 --select=E9 --ignore=E "$@"
 # if no pyflakes errors, run pep8
-flake8 --max-line-length=120 "$@"
+flake8 "$@"
