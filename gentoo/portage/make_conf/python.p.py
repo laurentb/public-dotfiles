f06e500c7f2efb761106226281d20efc3e0dc42e
Laurent Bachelier <laurent@bachelier.name>
2013-08-09 21:26:03 +0200

    Also set USE_PYTHON

--- gentoo/portage/make_conf/python.p.py
+++ gentoo/portage/make_conf/python.p.py
@@ -5,11 +5,18 @@ pythons = subprocess.Popen(['eselect', '--brief', 'python', 'list'],
 pythons = [python.replace('.', '_') for python in pythons.splitlines()]
 assert len(pythons)
 
+defpython = subprocess.Popen(['eselect', '--brief', 'python', 'show'],
+        stdout=subprocess.PIPE).communicate()[0]  # capture stdout
+defpython = defpython.strip().replace('python', '')
+assert len(defpython)
+
 if __name__ == "__main__":
     print pythons
+    print defpython
     exit()
 
 
 text("""# $warning
 PYTHON_TARGETS="$pythons"
-""").render(pythons=" ".join(pythons))
+USE_PYTHON="$defpython"
+""").render(pythons=" ".join(pythons), defpython=defpython)
