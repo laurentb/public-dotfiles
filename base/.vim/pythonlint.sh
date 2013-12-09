24bbd8c7887b61e141b18d0db0f22afec75bacc9
Laurent Bachelier <laurent@bachelier.name>
2013-12-09 22:13:48 +0100

    Add isort to pythonlint

--- base/.vim/pythonlint.sh
+++ base/.vim/pythonlint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ue
 # fail on pyflakes errors
 flake8 --select=E9,F "$@"
-# if no pyflakes errors, run pep8
-flake8 "$@"
+# if no pyflakes errors, run isort + pep8
+set +e
+EXIT_STATUS=0
+isort -c "$@" || EXIT_STATUS=$?
+flake8 "$@" || EXIT_STATUS=$?
+exit $EXIT_STATUS
