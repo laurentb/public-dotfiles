6de3769207e3c0aaac411e953c249942a0565aa0
Laurent Bachelier <laurent@bachelier.name>
2013-12-19 10:31:05 +0100

    Quiet isort on success

--- base/.vim/pythonlint.sh
+++ base/.vim/pythonlint.sh
@@ -4,6 +4,7 @@ flake8 --select=E9,F "$@"
 # if no pyflakes errors, run isort + pep8
 set +e
 EXIT_STATUS=0
-isort -c "$@" || EXIT_STATUS=$?
+ISORT=$(isort -c "$@") || EXIT_STATUS=$?
+echo ${ISORT}|grep -v '^SUCCESS'
 flake8 "$@" || EXIT_STATUS=$?
 exit $EXIT_STATUS
