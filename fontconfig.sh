9df4c63be53275695ac099d9d6f495aa7826b032
Laurent Bachelier <laurent@bachelier.name>
2014-01-21 19:28:06 +0100

    Quieter operation

--- fontconfig.sh
+++ fontconfig.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xeu
+#!/bin/bash -eu
 cd $(dirname $0)
 [[ -f /usr/share/eselect/modules/fontconfig.eselect ]] && ./fontconfig.py
 [[ -f /usr/share/eselect/modules/infinality.eselect ]] && eselect infinality set nyx && eselect lcdfilter set nyx
