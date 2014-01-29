d980e601f33037a9e0e5eda7d5046b7bee35c117
Laurent Bachelier <laurent@bachelier.name>
2014-01-29 19:24:35 +0100

    Soft fail

--- fontconfig.sh
+++ fontconfig.sh
@@ -2,3 +2,4 @@
 cd $(dirname $0)
 eselect --brief modules|grep -q '^fontconfig$' && ./fontconfig.py
 eselect --brief modules|grep -q '^infinality$' && eselect infinality set nyx && eselect lcdfilter set nyx
+exit 0
