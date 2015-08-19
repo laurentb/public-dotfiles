1d1b9214a23d2fa9a2418713ea342f4d194bc5b4
Laurent Bachelier <laurent@bachelier.name>
2015-08-19 18:47:37 +0200

    Generate RUBY_TARGETS

---
+++ gentoo/portage/make_conf/ruby.p.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import subprocess
+
+try:
+    subprocess.check_output(['eselect', 'ruby', 'show'], stderr=subprocess.PIPE)
+    has_ruby = True
+except subprocess.CalledProcessError:
+    has_ruby = False
+
+if has_ruby:
+    rubies = subprocess.check_output(['eselect', '--brief', 'ruby', 'list'])
+    rubies = [ruby.split('(')[0].strip() for ruby in rubies.splitlines()]
+    assert len(rubies)
+
+if __name__ == "__main__":
+    print(rubies)
+    exit()
+
+
+if has_ruby:
+    text("""# $warning
+    RUBY_TARGETS="$rubies"
+    """).render(rubies=" ".join(rubies))
+else:
+    text("# No ruby installation found.\n").render()
