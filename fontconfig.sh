4b34bc2de46c1961117dd4f5a45c501059da24da
Laurent Bachelier <laurent@bachelier.name>
2017-07-15 20:46:01 +0200

    freetype update; drop infinality

--- fontconfig.sh
+++ fontconfig.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -eu
 cd $(dirname $0)
 eselect --brief modules|grep -q '^fontconfig$' && ./fontconfig.py
-eselect --brief modules|grep -q '^infinality$' && eselect infinality set nyx && eselect lcdfilter set nyx
 exit 0
