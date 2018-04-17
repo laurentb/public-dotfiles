63b26ec8dc69fb60a59b9cbb1a3f7ff836847da4
Laurent Bachelier <laurent@bachelier.name>
2018-04-17 16:02:13 +0200

    USE_PYTHON is deprecated

--- gentoo/portage/make_conf/python.p.py
+++ gentoo/portage/make_conf/python.p.py
@@ -10,23 +10,13 @@ python = subprocess.check_output(['eselect', '--brief', 'python', 'show'])
 python = python.replace('.', '_').strip()
 assert python
 
-py2 = subprocess.Popen(
-    ['eselect', '--brief', 'python', 'show', '--ABI', '--python2'],
-    stdout=subprocess.PIPE).communicate()[0].strip()
-py3 = subprocess.Popen(
-    ['eselect', '--brief', 'python', 'show', '--ABI', '--python3'],
-    stdout=subprocess.PIPE).communicate()[0].strip()
-assert len(py2) and len(py3)
-
 if __name__ == "__main__":
     print(pythons)
     print(python)
-    print(py2, py3)
     exit()
 
 
 text("""# $warning
 PYTHON_TARGETS="$pythons"
 PYTHON_SINGLE_TARGET="$python"
-USE_PYTHON="$py2 $py3"
-""").render(pythons=" ".join(pythons), py2=py2, py3=py3, python=python)
+""").render(pythons=" ".join(pythons), python=python)
