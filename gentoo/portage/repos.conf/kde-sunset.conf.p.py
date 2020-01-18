90c18fe62efcd8b24501c407593bd8e5a9c8fdb5
Laurent Bachelier <laurent@bachelier.name>
2020-01-18 22:44:25 +0100

    Migrate from layman to repos.conf and eselect-repository

---
+++ gentoo/portage/repos.conf/kde-sunset.conf.p.py
@@ -0,0 +1,4 @@
+if "desktop" in options.tags:
+    redirect("kde-sunset.conf")
+else:
+    ignore()
---
+++ gentoo/portage/repos.conf/sirjaren.conf.p.py
@@ -0,0 +1,4 @@
+if "desktop" in options.tags:
+    redirect("sirjaren.conf")
+else:
+    ignore()
